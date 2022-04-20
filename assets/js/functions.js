139a06298dd93f087ec95617648fe313a836f34e
Jorge Gil Rivas <jorged1509@gmail.com>
2022-04-20 18:26:41 +0200

    chris modal/media query

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -119,20 +119,25 @@ function createCard(array, container) {
 
 		var bandeAnnonce = document.createElement('h2');
 		bandeAnnonce.textContent = 'Bande Annonce';
+		var boxTitleImg = document.createElement('div')
+		boxTitleImg.classList = 'boxTitleImg'
+		boxTitleImg.id = 'boxTitleImg'
+
+
 
 		modale.appendChild(modaleContainer);
 		modalYoutube.appendChild(bandeAnnonce);
+		modaleContainer2.appendChild(boxTitleImg);
 		modaleContainer.appendChild(modaleContainer2);
 		modale.appendChild(close);
-		modaleContainer2.appendChild(imgModale);
-		imgModale.appendChild(movieImg);
 		modaleContainer2.appendChild(modaleText);
+		modaleContainer2.appendChild(imgModale);
+		boxTitleImg.appendChild(modaleText)
+		boxTitleImg.appendChild(imgModale)
+		imgModale.appendChild(movieImg);
 		modaleText.appendChild(title2);
 		modaleText.appendChild(date);
 		modaleText.appendChild(popularity);
-		modaleContainer2.appendChild(modaleSyp);
-		modaleSyp.appendChild(synopsisH2);
-		modaleSyp.appendChild(synopsis);
 
 		main.appendChild(modale);
 
